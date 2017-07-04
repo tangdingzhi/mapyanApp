30ab5c6fea17830d726069817cec0937928b2e20
teryy6 <237536235@qq.com>
2017-07-04 09:45:35 +0800

    add router seat
    seat

---
+++ src/components/seat/seat.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>座位</h1>
+</template>
+
+<script>
+export default {
+  name: 'seat',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import seat from '@/components/seat/seat'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },{
+      path: '/seat',
+      name: 'seat',
+      component: seat
     }
   ]
 })
