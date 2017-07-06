6eac4a9fb794c5c41b2374cce38248bcf1364817
xiebiao <442951730@qq.com>
2017-07-06 11:07:21 +0800

    完成登录注册

--- src/components/maoyanApp/reg.vue
+++ src/components/maoyanApp/reg.vue
@@ -1,22 +1,24 @@
 <template>
  <div class="login">
   <div class="theme">猫眼APP后台管理系统</div>
-  <div style="width:400px; height: 400px; border:1px solid #C0CCDA; margin: 0 auto; margin-top: 100px; background-color: #C0CCDA;"> 
+  <div style="width:360px; height: 310px; border:1px solid #C0CCDA; margin: 0 auto; margin-top: 20px; background-color: #C0CCDA;"> 
   <div class="loginbox">
   <div style="text-align: center; color: white; font-family: Microsoft YaHei; font-size: 25px;" >注册页面</div>
 <div class="username">
-  <el-input placeholder="请输入您的账号" v-model="input3" style="300px;">
+  <el-input placeholder="请输入您的账号"  v-model="username" style="300px;" @blur="quertIsReg(username)">
     <template slot="prepend">账号</template>
   </el-input>
 </div>
+<div class="isReg" :class="{Gcolor:color,Rcolor:!color}">{{isReg}}</div>
 <div class="password">
-  <el-input placeholder="请输入您的密码" v-model="input3">
+  <el-input placeholder="请输入您的密码" type="password" v-model="password" >
     <template slot="prepend">密码</template>
   </el-input>
 </div>
+<div class="isReg" style="color:#13CE66">{{isOk}}</div>
 </div>
 <div class="btn">
-  <el-button type="primary">注册</el-button>
+  <el-button type="primary" @click="reg(username,password)">注册</el-button>
   </div>
   
   </div>
@@ -24,8 +26,48 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'reg',
+  data(){
+    return{
+      username:"",
+      password:"",
+      isReg:"",
+      color:true,
+      isOk:""
+    }
+  },
+  methods:{
+    async quertIsReg(username){
+      const {
+        data
+      } = await axios.get("http://localhost:3000/users/isUse", {
+        params: {
+          username:username,         
+        }
+      })
+      if(!data){
+        this.isReg="此账号已被注册",
+        this.color=false
+      }else {
+        this.isReg="此账号可以注册",
+        this.color=true
+      }
+      
+    },
+    async reg(username,password){
+      const {
+        data
+      } = await axios.post("http://localhost:3000/users/reg", {
+          username:username,  
+          password:password       
+      })
+     if(data){
+      this.isOk="注册成功"
+     }
+    },
+  }
   
 }
 </script>
@@ -39,6 +81,19 @@ export default {
 body {
   font-family: Helvetica, sans-serif;
 }
+.isReg{
+  padding: 10px 0;
+  text-align: center;
+  font-family:"PingFang SC";
+  font-size: 14px;
+  height: 30px;
+}
+.Gcolor{
+  color: #13CE66;
+}
+.Rcolor{
+  color: #FF4949
+}
 .login{
   width: 100%;
   height: 300px;
@@ -46,7 +101,7 @@ body {
 }
 .login .loginbox{
   margin: 0 auto;
-  margin-top: 80px;
+  margin-top: 20px;
   width: 300px;
   height: 200px;
   background-color: #C0CCDA;
@@ -64,7 +119,7 @@ body {
   margin-top: 30px;
 }
 .password{
-  margin-top: 30px;
+  /*margin-top: 30px;*/
 }
 .btn{
   text-align: center;
